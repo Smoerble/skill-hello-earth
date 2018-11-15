61a18d92a5ea17895a889be67776d3977a4bdf3f
Smo <smoerble@smoerble.de>
2018-11-15 22:31:34 +0100

    +fixed intention

--- __init__.py
+++ __init__.py
@@ -44,7 +44,7 @@ class HelloEarthSkill(MycroftSkill):
     #    self.register_intent(hello_earth_intent,
     #                         self.handle_hello_earth_intent)
 	
-    hello_earth_intent = IntentBuilder("HelloEarthIntent")
+        hello_earth_intent = IntentBuilder("HelloEarthIntent")
 			.require("HelloEarthKeyword")
             .require('phrase')
 			.build()
