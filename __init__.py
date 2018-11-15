7e329e7cc268b93ad377babe3225ce7d9fe49ff9
Smo <smoerble@smoerble.de>
2018-11-15 15:21:11 +0100

    more possibilities

--- __init__.py
+++ __init__.py
@@ -30,13 +30,13 @@ class HelloEarthSkill(MycroftSkill):
         super(HelloEarthSkill, self).__init__(name="HelloEarthSkill")
 
     def initialize(self):
-        thank_you_intent = IntentBuilder("ThankYouIntent"). \
-            require("ThankYouKeyword").build()
-        self.register_intent(thank_you_intent, self.handle_thank_you_intent)
+    #    thank_you_intent = IntentBuilder("ThankYouIntent"). \
+    #        require("ThankYouKeyword").build()
+    #    self.register_intent(thank_you_intent, self.handle_thank_you_intent)
 
-        how_are_you_intent = IntentBuilder("HowAreYouIntent"). \
-            require("HowAreYouKeyword").build()
-        self.register_intent(how_are_you_intent,
+    #    how_are_you_intent = IntentBuilder("HowAreYouIntent"). \
+    #        require("HowAreYouKeyword").build()
+    #    self.register_intent(how_are_you_intent,
                              self.handle_how_are_you_intent)
 
         hello_earth_intent = IntentBuilder("HelloEarthIntent"). \
@@ -44,11 +44,11 @@ class HelloEarthSkill(MycroftSkill):
         self.register_intent(hello_earth_intent,
                              self.handle_hello_earth_intent)
 
-    def handle_thank_you_intent(self, message):
-        self.speak_dialog("welcome")
+    #def handle_thank_you_intent(self, message):
+    #    self.speak_dialog("welcome")
 
-    def handle_how_are_you_intent(self, message):
-        self.speak_dialog("how.are.you")
+    #def handle_how_are_you_intent(self, message):
+    #    self.speak_dialog("how.are.you")
 
     def handle_hello_earth_intent(self, message):
         self.speak_dialog("hello.earth")
